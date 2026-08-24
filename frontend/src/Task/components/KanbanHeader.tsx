b05c7495f5d94872a4299d92caa14a65e45e41f6
Stefano Salidu <st.salidu@gmail.com>
2026-08-24 15:02:01 +0200

    feat: add Kanban header

---
+++ frontend/src/Task/components/KanbanHeader.tsx
@@ -0,0 +1,11 @@
+type HeaderProps = {
+    className?: string;
+}
+
+export default function Header({ className }: HeaderProps) {
+    return <div className={`flex justify-between items-center p-4 ${className}`}>
+        <div className="flex flex-col items-start gap-4">
+            <h1 className="text-3xl font-bold text-white">KanbAIn</h1>
+        </div>
+    </div>
+}
